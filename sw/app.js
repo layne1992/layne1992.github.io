7e5b4ad0583e3501dc7794d10ba4c6d6c2c0d563
layne1992 <begin-is-end@foxmail.com>
2017-06-20 14:55:01 +0800

    Update app.js

--- sw/app.js
+++ sw/app.js
@@ -2,7 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw/sw.js', { scope: '/sw/' }).then(function(reg) {
-
+    
     if(reg.installing) {
       console.log('Service worker installing');
     } else if(reg.waiting) {
@@ -10,7 +10,7 @@ if ('serviceWorker' in navigator) {
     } else if(reg.active) {
       console.log('Service worker active');
     }
-
+    reg.update();
   }).catch(function(error) {
     // registration failed
     console.log('Registration failed with ' + error);
